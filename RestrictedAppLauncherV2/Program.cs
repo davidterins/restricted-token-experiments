f185dc24913a7ca8e197e8edd6d05271e24e8790
David Terins <david.terins@beijerelectronics.com>
2024-10-17 11:40:23 +0200

    wip

--- RestrictedAppLauncherV2/Program.cs
+++ RestrictedAppLauncherV2/Program.cs
@@ -7,8 +7,11 @@ using var tokenProvider = TokenProvider
     .RestrictToken();
 
 //var applicationPath = @$"{AppDomain.CurrentDomain.BaseDirectory}AppToLaunch\AppToLaunch.exe";
-var applicationPath = @$"{AppDomain.CurrentDomain.BaseDirectory}WebViewApp\WebViewApp.exe";
-var workingDirectory = Directory.GetParent(applicationPath).FullName;
+//var applicationPath = @$"{AppDomain.CurrentDomain.BaseDirectory}WebViewApp\WebViewApp.exe";
+var applicationPath = @"C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe";
+//C:\Program Files(x86)\Microsoft\Edge\Application
+//var workingDirectory = Directory.GetParent(applicationPath).FullName;
+var workingDirectory = @"C:\Program Files (x86)\Microsoft\Edge\Application";
 
 ProcessExtensions.CreateProcessAsUser(
     tokenProvider.GetToken(),
